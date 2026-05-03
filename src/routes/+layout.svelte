f3626a0e6961c63b1d7eae0659be00b4adf07bbb
Jonatan Gezelius <boolean.adapter@gmail.com>
2026-05-03 12:27:55 +0200

    Move global styles from +layout to app.css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,7 @@
 	import { getAlternateLinks } from '$lib/seo';
 	import SiteHeader from '$lib/components/SiteHeader.svelte';
 	import SiteFooter from '$lib/components/SiteFooter.svelte';
+	import '../app.css';
 
 	const { data, children } = $props();
 
@@ -62,56 +63,6 @@
 </svelte:head>
 
 <style>
-	:global(:root) {
-		--container-width-default: 1080px;
-		--container-width-wide: 1140px;
-		--container-width-narrow: 980px;
-		--container-gutter: 2rem;
-		--text-width: 54rem;
-		--page-title-width: 20ch;
-		--page-title-size: clamp(3rem, 8vw, 5.6rem);
-		--page-title-line-height: 0.95;
-	}
-
-	:global(html, body) {
-		min-height: 100%;
-	}
-
-	:global(body) {
-		margin: 0;
-		font-family: 'Roboto', system-ui, sans-serif;
-		color: #10231c;
-		background: url('/mandalon/motherboard-bg.jpg') center top / cover no-repeat fixed;
-	}
-
-	:global(h1, h2, h3, h4, h5, h6) {
-		font-family: 'Roboto Condensed', 'Roboto', system-ui, sans-serif;
-	}
-
-	:global(img) {
-		max-width: 100%;
-		display: block;
-	}
-
-	:global(a) {
-		color: inherit;
-	}
-
-	:global(.container) {
-		width: min(var(--container-width, var(--container-width-default)), calc(100vw - var(--container-gutter)));
-		margin: 0 auto;
-	}
-
-	:global(.text-width) {
-		max-width: var(--text-width);
-	}
-
-	:global(.page-title) {
-		max-width: var(--page-title-width);
-		font-size: var(--page-title-size);
-		line-height: var(--page-title-line-height);
-	}
-
 	.app-shell {
 		min-height: 100vh;
 	}
